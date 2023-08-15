535177890a45148d6518b694457fbace9ecd645a
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-15 09:25:51 +0200

    add hero service to dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  private getHeroes() {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1,5))
+  }
 }
