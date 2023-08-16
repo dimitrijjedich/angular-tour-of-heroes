d765ee7ff21e659afb0ce23f3eb09de7eab5aa58
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-16 09:42:31 +0200

    add genId to in-memory-data service

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -22,4 +22,9 @@ export class InMemoryDataService implements InMemoryDbService{
       { id: 20, name: 'Tornado' }
     ];
   }
+
+  // Returns the highest hero id+1 or 11 as initial value
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
 }
