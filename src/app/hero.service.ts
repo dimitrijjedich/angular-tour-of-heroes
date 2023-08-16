73108c316e5b1a1159354e65081ac6c9bd622804
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-16 20:10:50 +0200

    import HttpClient and HttpHeaders into hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { Hero } from "./hero";
 import { HEROES } from './mock-heroes';
 import { MessageService } from "./message.service";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
